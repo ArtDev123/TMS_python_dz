461846fe3bed5a4b02efa01da671884c097bb791
ksenialapuskova7-star <ksenialapuskova7@gmail.com>
2026-06-05 00:30:34 +0300

    ff

---
+++ ksusha/дз21_05/program1.py
@@ -0,0 +1,28 @@
+class Soda:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def get_taste(self) -> None:
+        taste = input(f"Введите вкус {self.name} или просто нажмите Enter," \
+        " если у газировки нет вкуса: ")
+        self.taste = taste    
+
+    def print_soda_taste(self) -> None:
+        if self.taste == "":
+            print(f"{self.name} имеет обычный вкусом")
+        else:
+            print(f"{self.name} имеет вкус: {self.taste}")
+
+sodas : list[Soda] = []
+count = int(input("Введите количество газировок, которые хотите добавить: "))
+
+
+for i in range(count):
+    name = input("Введите название газировки: ")
+    sodas.append(Soda(name))
+
+for soda in sodas:
+    soda.get_taste()
+
+for soda in sodas:
+    soda.print_soda_taste()
