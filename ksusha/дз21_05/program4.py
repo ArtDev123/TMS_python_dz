f8e0a54aa52c0b8e21287908123075c6ee5eaa40
ksenialapuskova7-star <ksenialapuskova7@gmail.com>
2026-06-04 14:00:23 +0300

    dd

--- ksusha/дз21_05/program4.py
+++ ksusha/дз21_05/program4.py
@@ -2,24 +2,25 @@ from math import pi
 
 
 class Sphere:
-    def __init__(self, radius=1, x=0, y=0, z=0) -> None:
+    def __init__(self, radius: float = 1, x: float = 0,
+                 y: float = 0, z: float = 0) -> None:
         self.radius = radius
         self.x = x
         self.y = y
         self.z = z
 
     def get_volume(self) -> float:
-        v = (4*pi*(self.radius**3))/3
+        v: float  = (4*pi*(self.radius**3))/3
         return v
 
     def get_square(self) -> float:
-        s = 4*pi*(self.radius**2)
+        s: float = 4*pi*(self.radius**2)
         return s
 
     def get_radius(self) -> float:
         return self.radius
 
-    def get_center(self) -> float:
+    def get_center(self) -> tuple[float, float, float]:
         return (self.x, self.y, self.z)
 
     def set_radius(self, new_radius: float) -> None:
@@ -31,4 +32,5 @@ class Sphere:
         self.z = z_new
 
     def is_point_inside(self, x_point: float, y_point: float, z_point: float) -> bool:
-        return (self.x-x_point)**2 + (self.y-y_point)**2 + (self.z-z_point)**2 < self.radius**2
+        dd = (self.x-x_point)**2 + (self.y-y_point)**2 + (self.z-z_point)**2 < self.radius**2
+        return dd
